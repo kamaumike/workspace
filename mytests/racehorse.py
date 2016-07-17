84d273b70e6f4dabe6c2f5bc055c38c5938f5742
kamaumike <kamaumike2013@gmail.com>
2016-07-17 17:06:40 +0300

    created a sub class of horse

---
+++ mytests/racehorse.py
@@ -0,0 +1,17 @@
+from horse import Horse
+
+class RaceHorse(Horse):
+	"""A faster horse that inherits from Horse"""
+	def movement(self):
+		return "run"
+	def movement_slow(self):
+		return super(Horse,self).movement()
+	def __repr__(self):
+		return "%s race horse weighing %f and wild status is %b" (self.color, self.weight, self.wild)	
+
+
+horse3 = RaceHorse("white", 200)		
+
+print horse3.movement_slow()
+print horse3.movement()
+		
